d57690b2e8ecbb52d4dc1b5d1074e6383bdcb6f7
Kevin Bridges <kevin@wiiBridges.com>
2019-02-28 09:14:45 -0600

    added support for sub-domain deployment: https://eatery-nod-w.js.org

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ class App extends Component {
           <p>
           WowZee WowZee WooWoo ... changes by kevin
           </p>
+          <p>
+          WomBee WomBee WooLoo ... changes by kevin
+          </p>
           <a className="App-link"
              href="https://reactjs.org"
              target="_blank"
