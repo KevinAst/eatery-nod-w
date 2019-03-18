2337dec4d05aa65e230201b3a9cd45b96ef49623
Kevin Bridges <kevin@wiiBridges.com>
2019-03-18 15:45:46 -0500

    adding BASE utilities

---
+++ src/util/ErrorExtensionPolyfill.js
@@ -0,0 +1,217 @@
+/**
+ * Standardize the ability to handle ALL errors more generically, by
+ * adding these value-added extensions to ALL Error objects (via this
+ * Error class polyfill - a monkey patch).
+ * 
+ * All Error instances (created anywhere) are extended to include the
+ * following characteristics:
+ * 
+ * - A new error.userMsg property is defined.
+ * 
+ *   This message is intended to be seen by users, so it should be
+ *   suitable for human consumption:
+ *     - both in meaning, 
+ *     - and in sanitation (so as to not reveal any internal
+ *       architecture)
+ * 
+ *   By default, error.userMsg = 'Unexpected Condition'
+ *   and can be changed by: 
+ *     + error.defineUserMsg(userMsg): error
+ * 
+ * - There is a delineation of expected vs. unexpected conditions.  For
+ *   example:
+ * 
+ *     - An error communicating "invalid password" is an expected
+ *       condition, because it is stimulated by user input.
+ * 
+ *     - Contrast that with an error communicating "DB is down", which is
+ *       an unexpected condition, because this is outside the user's
+ *       control.
+ * 
+ *   This distinction is controlled by whether an error.userMsg has
+ *   been defined (expected) or not (unexpected).
+ * 
+ *   In other words, all errors start out as unexpected, and can be
+ *   changed to expected by simply invoking:
+ *     + error.defineUserMsg(userMsg): error
+ * 
+ *   This distinction may be programmatically delineated through one of
+ *   the following methods:
+ *     + error.isExpected(): boolean
+ *     + error.isUnexpected(): boolean
+ * 
+ * - A new error.attemptingToMsg property is defined.
+ * 
+ *   This message is also intended to be seen by users, and provides
+ *   additional detail of what was being attempted (over and above the
+ *   error.userMsg).
+ * 
+ *   By default, error.attemptingToMsg = ''
+ *   and can be changed by: 
+ *     + error.defineAttemptingToMsg(attemptingToMsg): error
+ * 
+ * - A new error.formatUserMsg() is provided that combines all
+ *   user-specific messages (userMsg and attemptingToMsg).
+ * 
+ *     + error.formatUserMsg(): string
+ * 
+ * - The toString() method has been extended to suffix the base
+ *   toString() with user-specific messages.
+ * 
+ * - All of the new "defining" methods return the receiving error,
+ *   so as to allow them to be conveniently chained.  For example:
+ * 
+ *     throw new Error('catastrophic details here').defineAttemptingToMsg('sign in');
+ * 
+ * **Usage Scenarios** are as follows:
+ * 
+ * - Error Origination
+ * 
+ *   In throwing a new Error, you can:
+ *     throw new Error('internal technical error details')
+ *                 .defineUserMsg('You did not bla')           // ONLY INVOKE if this is an expected condition - otherwise default to: 'Unexpected Condition'
+ *                 .defineAttemptingToMsg('log into the app'); // optionally provide additional clarification in either case (expected/unexpected)
+ * 
+ * - Error Pass Through
+ * 
+ *   Within a mid-level service, you may capture an error from a lower
+ *   point and supplement it as follows:
+ * 
+ *     catch(err) {
+ *       throw err.defineUserMsg('You did not bla')           // ONLY INVOKE if this is an expected condition - otherwise default to: 'Unexpected Condition'
+ *                .defineAttemptingToMsg('log into the app'); // optionally provide additional clarification in either case (expected/unexpected)
+ *     }
+ * 
+ * - Error Consumption (by client)
+ * 
+ *   Using these enhancements, the client can abstractly apply various
+ *   heuristics, such as:
+ *
+ *     - if logging is necessary
+ *       * if so, reveal complete context (internal details and user context)
+ *     - if user notification necessary
+ *       * if so supply info suitable for human consumption
+ * 
+ *   For more usage scenarios, please refer to the discloseError.js utility.
+ */
+
+
+if (!Error.prototype.defineUserMsg) { // key off of one of several extension points
+
+  /**
+   * Define a user-specific message, that is applicable for human
+   * consumption:
+   *  - both in meaning, 
+   *  - and in sanitization (so as to not reveal any internal architecture).
+   *
+   * This method also delineates the error as an expected condition.
+   *
+   * @param {String} userMsg the user message to define
+   *
+   * @return {Error} self, supporting convenient Error method chaining.
+   */
+  Error.prototype.defineUserMsg = function(userMsg) {
+    this.userMsg  = userMsg;
+    this.expected = true;
+    return this;
+  };
+  Error.prototype.userMsg = "Unexpected Condition"; // prototype provides the default
+
+
+  /**
+   * Return an indicator as to whether this error was
+   * expected (say user input error),
+   * or not (say a catestrophic error).
+   *
+   * @return {boolean} error expected (true) or not (false).
+   */
+  Error.prototype.isExpected = function() {
+    return this.expected;
+  };
+  Error.prototype.expected = false; // prototype provides the default
+
+
+  /**
+   * Return an indicator as to whether this error was
+   * unexpected (say a catestrophic error),
+   * or not (say user input error).
+   *
+   * @return {boolean} error unexpected (true) or not (false).
+   */
+  Error.prototype.isUnexpected = function() {
+    return !this.expected;
+  };
+
+
+  /**
+   * Define a user-specific 'attempting to' message, that provides
+   * additional details of what was being attempted.
+   *
+   * Errors with this context are prefixed with ' ... attempting to: ',
+   * so word your phrasing appropriatly.
+   * 
+   * Multiple attempting-to phrases can be used, which will be
+   * combined with the ', -and- ' phrase.
+   *
+   * @param {String} attemptingToMsg the user-specific attempting
+   * to' message.
+   *
+   * @return {Error} self, supporting convenient Error method chaining.
+   */
+  Error.prototype.defineAttemptingToMsg = function(attemptingToMsg) {
+    if (this.attemptingToMsg) // append multiples
+      this.attemptingToMsg += `, -and- ${attemptingToMsg}`;
+    else                      // initial definition
+      this.attemptingToMsg += ` ... attempting to: ${attemptingToMsg}`;
+    return this;
+  };
+  Error.prototype.attemptingToMsg = ''; // prototype provides the default
+
+
+  /**
+   * Format a user-specific message, combining all user-specific contexts.
+   *
+   * @return {string} formatted user message.
+   */
+  Error.prototype.formatUserMsg = function() {
+    return this.userMsg + this.attemptingToMsg;
+  };
+
+
+  /**
+   * Extend the Error toString() to prefix user-specfic context.
+   */
+  const prior_toString = Error.prototype.toString; // monkey patch
+  Error.prototype.toString = function() {
+    return prior_toString.call(this) + '\n\nUser Msg: ' + this.formatUserMsg();
+  };
+
+
+  // L8TR:
+  // /**
+  //  * Define an indicator as to the cause of this error ... used to apply
+  //  * various heuristics, such as whether logging is necessary.
+  //  *
+  //  * The following indicators are available:
+  //  *   Error.Cause {
+  //  *     UNEXPECTED_CONDITION        [default]
+  //  *     RECOGNIZED_USER_ERROR
+  //  *   }
+  //  *
+  //  * @param {String} cause one of Error.Cause.
+  //  *
+  //  * @return {Error} self, supporting convenient Error method chaining.
+  //  */
+  // Error.prototype.defineCause = function(cause) {
+  //   this.cause = cause;
+  //   return this;
+  // };
+  // 
+  // Error.Cause = {
+  //   UNEXPECTED_CONDITION:    'UNEXPECTED_CONDITION',
+  //   RECOGNIZED_USER_ERROR: 'RECOGNIZED_USER_ERROR'
+  // };
+  // 
+  // Error.prototype.cause = Error.Cause.UNEXPECTED_CONDITION; // prototype provides the default
+
+}
---
+++ src/util/discloseError.js
@@ -0,0 +1,118 @@
+import {toast, alert} from './notify';
+import verify         from './verify';
+
+const DYNAMIC_DEFAULT = 'DYNAMIC_DEFAULT';
+
+/**
+ * The discloseError() utility provides a standard way to 
+ * disclose errors both to the user (toasts) and logs.
+ *
+ * This utility uses the heuristics defined by the
+ * ErrorExtensionPolyfill, and therefore require it to be present.
+ *
+ * The emission of both user notifications and logs can be controlled,
+ * through parameters that dynamically default (based on "expected"
+ * vs. "unexpected" error status).
+ *
+ * All user notifications occur through toasts, and are sanitized:
+ *  - showing appropriate user-level detail,
+ *  - with more information available via a button click
+ *
+ * Log formats are also standardized.
+ *
+ * **Please Note** this function uses named parameters.
+ *
+ * @param {Error} err the Error object to be disclosed (logged and
+ * user-notified).
+ *
+ * @param {boolean} [showUser=true] an indicator as to whether the
+ * user should be shown this error.
+ *
+ * @param {boolean} [logIt=DYNAMIC_DEFAULT] an indicator as to whether
+ * a log entry should be emitted, dynamically defaulted (LOG when err
+ * is Unexpected, NO-LOG when Expected).
+ */
+export default function discloseError({err,
+                                       showUser=true,         // DEFAULT: true ................. i.e. always SHOW
+                                       logIt=DYNAMIC_DEFAULT, // DEFAULT: err.isUnexpected() ... i.e. LOG when Unexpected, NO-LOG when Expected
+                                       ...unknownArgs}={}) {
+
+  // validate parameters
+  try {
+    const check = verify.prefix('discloseError() parameter violation: ')
+    // ... err
+    check(err,           'err is required');
+    check(err instanceof Error, 'err must be an Error object, NOT: ', err);
+    // ... showUser
+    showUser = showUser===DYNAMIC_DEFAULT ? err.isExpected() : showUser; // dynamic DEFAULT semantics
+    check(showUser===true || showUser===false, 'showUser must be a boolean');
+    // ... logIt
+    logIt = logIt===DYNAMIC_DEFAULT ? err.isUnexpected() : logIt; // dynamic DEFAULT semantics
+    check(logIt===true || logIt===false, 'logIt must be a boolean');
+    // ... unrecognized named parameter
+    const unknownArgKeys = Object.keys(unknownArgs);
+    check(unknownArgKeys.length === 0,  `unrecognized named parameter(s): ${unknownArgKeys}`);
+    // ... unrecognized positional parameter (NOTE: when defaulting entire struct, arguents.length is 0)
+    check(arguments.length === 0 || arguments.length === 1, 'unrecognized positional parameters (only named parameters can be specified)');
+  }
+  // NOTE: this handler CANNOT throw an error, 
+  //       because it is typically used inside a promise.catch()
+  //       which will generate an "Unhandled promise rejection"
+  catch(e) {
+    console.log('YIKES ... eatery-nod-w\'s discloseError() was attempting to report on err: ', err);
+    console.log('HOWEVER AN INVOCATION PROBLEM was detected in the parameters passed to discloseError()!\n' +
+                '... discloseError() CANNOT throw an error (when used in a promise an "Unhandled promise rejection" will result)!!\n' +
+                '... PLEASE FIX THE FOLLOWING INVOCATION PROBLEM: ', e);
+    return; // cannot throw(e) ... see log (above)
+  }
+
+  // show user when requested
+  if (showUser) {
+    // unexpeded errors display as error toasts with additional details link
+    if (err.isUnexpected()) {
+      toast.error({  // ... will auto close -OR- when "details" is clicked
+        msg: err.formatUserMsg(),
+        actions: [
+          { txt:    'details',
+            action: () => {
+              alert.error({ msg: `An unexpected error occurred:
+
+${err}
+
+If this problem persists, please contact your tech support.`
+              });
+            }},
+        ]
+      });
+    }
+
+    // expeded errors display as warning toasts with NO additional detail
+    else {
+      toast.warn({  // ... will auto close
+        msg: err.formatUserMsg()
+      });
+    }
+        
+  }
+
+  // generate log entry when requested
+  if (logIt) {
+
+    // log the details of the error (with traceback) for tech review
+    // NOTE 1: we refrain from using console.warn() and console.error() 
+    //         because of BAD semantics inferred by react-native/expo:
+    //          - console.warn():  generates yellow popup
+    //          - console.error(): kills app with "red screen of death"
+    // NOTE 2: react-native/expo appears to be doing something non standard
+    //         when passing error as the 2nd parameter of log():
+    //             log('msg', error)
+    //         ... in this case is merely emitting error.toString()
+    //         ... we can however receive a stack trace by calling log(error) as the first parameter
+    const prefix = err.isUnexpected() ? '*** Unexpected Error:\n\n' : '*** Expected Error:\n\n';
+    console.log(prefix + err);
+    if (err.isUnexpected()) { // produce stack traces only for unexpected errors
+      console.log(err);
+    }
+  }
+
+}
