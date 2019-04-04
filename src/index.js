463743946af81d32780f84d043ab5414e5d78f3e
Kevin Bridges <kevin@wiiBridges.com>
2019-04-04 13:37:06 -0500

    remove old src/util/layout items (initial template stuff)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-// redirect to app.js
-// export {default} from './app';
-import './app';
-
-
-
-
-// import './index_OBSOLETE'; // ?? temporary test of boilerplate stuff
-
-
+export {default} from './app'; // redirect to app.js
