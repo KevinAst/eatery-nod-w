cfdfc793a9e1bab1512cbb697f2af4c983a14ec3
Kevin Bridges <kevin@wiiBridges.com>
2019-04-22 12:01:39 -0500

    remove sensitive file (after checked in)

--- src/features/misc/firebaseInit/config/firebaseAppConfig.js
+++
@@ -1,3 +0,0 @@
-// The eatery-nod FireBase App Configuration (a PRIVATE RESOURCE in a gitignored resource)
-// NOTE: We encode sensitive credential keys to alleviate exposure on deployment site!
-export default 'theireyJhcGlLZXkiOiJBSXphU3lDZmpPMXNvUDFBOThLVnlsQ2JyRC11M2ROVGx1ZWdhM0UiLCJhdXRoRG9tYWluIjoiZWF0ZXJ5LW5vZC5maXJlYmFzZWFwcC5jb20iLCJkYXRhYmFzZVVSTCI6Imh0dHBzOi8vZWF0ZXJ5LW5vZC5maXJlYmFzZWlvLmNvbSIsInByb2plY3RJZCI6ImVhdGVyeS1ub2QiLCJzdG9yYWdlQnVja2V0IjoiZWF0ZXJ5LW5vZC5hcHBzcG90LmNvbSIsIm1lc3NhZ2luZ1NlbmRlcklkIjoiMTIzNjg3ODQzNzM2In0=';
