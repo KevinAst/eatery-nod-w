9b5d3fc2eb05c9757b8d0ed23c2b6bd04c39bdd1
Kevin Bridges <kevin@wiiBridges.com>
2019-04-26 09:30:08 -0500

    minor comment clarifications

--- src/features/index.js
+++ src/features/index.js
@@ -1,32 +1,33 @@
-import eateries                      from './eateries/feature';
-import eateryService                 from './eateries/subFeatures/eateryService/feature';
-import eateryServiceFirebase         from './eateries/subFeatures/eateryServiceFirebase/feature';
-import eateryServiceMock             from './eateries/subFeatures/eateryServiceMock/feature';
-
-import discovery                     from './discovery/feature';
-import discoveryService              from './discovery/subFeatures/discoveryService/feature';
-import discoveryServiceGooglePlaces  from './discovery/subFeatures/discoveryServiceGooglePlaces/feature';
-import discoveryServiceMock          from './discovery/subFeatures/discoveryServiceMock/feature';
-
 import auth                          from './common/auth/feature';
 import authService                   from './common/auth/subFeatures/authService/feature';
 import authServiceFirebase           from './common/auth/subFeatures/authServiceFirebase/feature';
 import authServiceMock               from './common/auth/subFeatures/authServiceMock/feature';
 import bootstrap                     from './common/bootstrap/feature';
+import discovery                     from './discovery/feature';
+import discoveryService              from './discovery/subFeatures/discoveryService/feature';
+import discoveryServiceGooglePlaces  from './discovery/subFeatures/discoveryServiceGooglePlaces/feature';
+import discoveryServiceMock          from './discovery/subFeatures/discoveryServiceMock/feature';
+import eateries                      from './eateries/feature';
+import eateryService                 from './eateries/subFeatures/eateryService/feature';
+import eateryServiceFirebase         from './eateries/subFeatures/eateryServiceFirebase/feature';
+import eateryServiceMock             from './eateries/subFeatures/eateryServiceMock/feature';
 import initFirebase                  from './common/initFirebase/feature';
 import initGooglePlaces              from './common/initGooglePlaces/feature';
 import layout                        from './common/layout/feature';
 import location                      from './common/location/feature';
-import pwa                           from './common/pwa/feature';
-
 import logActions                    from './common/diagnostic/logActions/feature';
+import pwa                           from './common/pwa/feature';
 import sandbox                       from './common/diagnostic/sandbox/feature';
 
-// accumulate ALL features
-// ... see README.md for details
+// accumulate/promote ALL features that make up our app
 export default [
 
-  eateries, // ... ORDER: before discovery feature to promote Pool left-nav FIRST
+
+  //***
+  //*** app-specific features
+  //***
+
+  eateries, // ... ORDER: before `discovery` feature to promote Pool left-nav FIRST
   eateryService,
   eateryServiceFirebase,
   eateryServiceMock,
@@ -36,19 +37,28 @@ export default [
   discoveryServiceGooglePlaces,
   discoveryServiceMock,
 
-  // ... common
-  layout, // ... ORDER: before auth feature to promote UITheme user-menu FIRST
+
+  //***
+  //*** common app-neutral features
+  //***
+
+  layout, // ... ORDER: before `auth` feature to promote UITheme user-menu FIRST
+  
   auth,
   authService,
   authServiceFirebase,
   authServiceMock,
+  
   bootstrap,
+  
   initFirebase,
   initGooglePlaces,
+  
   location,
+  
   pwa,
 
-  // ... common diagnostic
+  // diagnostic features ...
   logActions,
   sandbox,
 ];
