99f4469fd65e4ac1f873301366d63f2d88f0a25a
Kevin Bridges <kevin@wiiBridges.com>
2019-04-30 18:08:29 -0500

    Updates

---
+++ precache-manifest.150cd3ffc7b062a99cab05afbb133b6b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "88f7111998a64b9ecab7",
+    "url": "/KevinAst/eatery-nod-w/static/js/runtime~main.88f71119.js"
+  },
+  {
+    "revision": "74ee31ce79c72b2f764d",
+    "url": "/KevinAst/eatery-nod-w/static/js/main.74ee31ce.chunk.js"
+  },
+  {
+    "revision": "c2a2ef29a38b6365c598",
+    "url": "/KevinAst/eatery-nod-w/static/js/2.c2a2ef29.chunk.js"
+  },
+  {
+    "revision": "3fd2389e82fe5b3f7b8e6f9457a2012f",
+    "url": "/KevinAst/eatery-nod-w/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60c7e0b3e29efb1da7ed59958e7464bc.js"
+  "/KevinAst/eatery-nod-w/precache-manifest.150cd3ffc7b062a99cab05afbb133b6b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/KevinAst/eatery-nod-w/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
