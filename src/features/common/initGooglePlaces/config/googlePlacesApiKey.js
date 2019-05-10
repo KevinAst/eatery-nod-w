e5999a33d7f4e4dccff6d5e50e59ebca355cbae1
Kevin Bridges <kevin@wiiBridges.com>
2019-05-10 16:29:36 -0500

    fix .gitignore

--- src/features/common/initGooglePlaces/config/googlePlacesApiKey.js
+++
@@ -1,3 +0,0 @@
-// The eatery-nod Google Places API Key (a PRIVATE RESOURCE in a gitignored resource)
-// NOTE: We encode sensitive credential keys to alleviate exposure on deployment site!
-export default 'afesaQUl6YVN5RHZiNlBiXzV0RjVHN01jNTQ1QnFiQ1g4SmtXWk9LQlVz';
