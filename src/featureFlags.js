a560e906d1f6fc26724560cc7672713f214db372
Kevin Bridges <kevin@wiiBridges.com>
2019-06-13 15:32:59 -0500

    mock GPS (baseline for demo)

--- src/featureFlags.js
+++ src/featureFlags.js
@@ -6,7 +6,7 @@ export default {
 
   useWIFI: true,  // should app use WIFI? ... regulates various services: real/mocked
 
-  mockGPS: false, // should app mock GPS? ... regulates util/deviceLocation.js getCurPos():  real/mocked
+  mockGPS: true,  // should app mock GPS? ... regulates util/deviceLocation.js getCurPos():  real/mocked
                   // false: use real device location
                   // true:  mock device location (default to Glen Carbon IL)
                   // {lat, lng}: specify mocked location, example:
